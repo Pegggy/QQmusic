86c6da45872f2e91becc50249dafc03e986a3232
penfan <fangpei9212@163.com>
2017-10-06 17:14:47 +0800

    引入首页轮播

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,30 @@
 (function () {
-  let slider = new Slider({
-    ct: document.querySelector('#slider'),
+  let slider = {
     slides: [
-      { link: '#', img: 'imgs/slide1.jpg' },
-      { link: '#', img: 'imgs/slide2.jpg' },
-      { link: '#', img: 'imgs/slide3.jpg' },
-      { link: '#', img: 'imgs/slide4.jpg' },
-      { link: '#', img: 'imgs/slide5.jpg' }
+      {link: '#', img: 'imgs/slide1.jpg'},
+      {link: '#', img: 'imgs/slide2.jpg'},
+      {link: '#', img: 'imgs/slide3.jpg'},
+      {link: '#', img: 'imgs/slide4.jpg'},
+      {link: '#', img: 'imgs/slide5.jpg'}
     ]
-  })
+  };
+  let $swiper = document.querySelector('.swiper-container');
+  $swiper.innerHTML = ' <div class="swiper-wrapper"></div><div class="swiper-pagination" id="swiper-pagination"></div>';
+  let $swiperWrapper = $swiper.querySelector('.swiper-wrapper');
+  $swiperWrapper.innerHTML = slider.slides.map(slide =>
+    `<div class="swiper-slide">
+      <a href="${slide.link}" >
+      <img src="${slide.img}" style="width: 100%;height: 100%;">
+      </a>
+    </div>`
+  ).join('');
+  let mySwiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    autoplay : 3000,
+    speed: 400,
+    pagination : '.swiper-pagination',
+    loop: true,
+    setWrapperSize :true
+  });
 })()
+
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
+        exclude: /(node_modules|src)/,
         use: ['babel-loader?cacheDirectory=true'],
         include: path.join(__dirname,'scripts')
       }]
