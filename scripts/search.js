516d04130b99023720077c05234a1ffcdcf3af1f
penfan <fangpei9212@163.com>
2017-10-11 22:48:48 +0800

    完成滚动加载

--- scripts/search.js
+++ scripts/search.js
@@ -7,10 +7,15 @@ export class Search{
     this.$input.addEventListener('focus',this.onFocus.bind(this));
     this.$cancelBtn.addEventListener('click',this.onBtnClick.bind(this));
     this.$input.addEventListener('keyup',this.onKeyUp.bind(this));
+    
     this.keyword = '';
     this.page = 1;
     this.perpage = 20;
     this.cancel = false;
+    this.end = false;
+    this.isLoading = false;
+    window.addEventListener('scroll',this.onScroll.bind(this))
+
   }
   onFocus(event){
     this.$ct.querySelector('.hot-search').classList.add('hide');
@@ -29,43 +34,68 @@ export class Search{
   onKeyUp(event){
     let keyword = event.target.value.trim();
     if(!keyword) this.reset();
-    if(event.keyCode !== 13) return
+    if(event.keyCode !== 13 || this.isLoading ) return
     this.search(keyword)
+  }
+  onScroll(event){
+    if(this.end) {
+      this.$ct.querySelector('.nomore').classList.remove('hide');
+      return
+    }
+    if(this.isLoading){
+      return
+    }
+    if(document.documentElement.clientHeight + window.pageYOffset > document.body.scrollHeight - 50 ){
+      this.search(this.keyword, this.page + 1 )
+    }
   }
   reset(){
     this.keyword = '';
     this.page = 1;
     this.$songs.innerHTML = '';
   }
-  search(keyword){
+  search(keyword,page){
     this.onFocus();
+    this.isLoading = true;
+    this.showLoading()
     this.keyword = keyword;
-    fetch(`http://localhost:4000/search?keyword=${this.keyword}&page=${this.page}`)
+    fetch(`http://localhost:4000/search?keyword=${this.keyword}&page=${page||this.page}`)
       .then(res => res.json())
       .then(json => {
-        this.renderSinger(json.data.zhida);
-        this.renderResult(json)
+        this.page = json.data.song.curpage;
+        // this.end = json.data.message === "";
+        return json.data.song.list;
       })
+      .then(list => this.renderResult(list))
+      .then(() => {
+        this.isLoading = false;
+        this.hideLoading();
+      })
+    }
+  showLoading(){
+    this.$ct.querySelector('.loading').classList.remove('hide');
   }
-  renderSinger(zhidao){
-    let imgUrl = zhidao.singermid;
-    let html = `
-      <li class="singer-info clearfix">
-        <span>
-          <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${imgUrl}.jpg?max_age=2592000">
-        </span>
-        <h6 class="result-title">${zhidao.singername}</h6>
-        <p class="result-content text-hide">
-          <span>单曲：${zhidao.singerid}</span>
-          <span>专辑：${zhidao.albumnum}</span>
-        </p>
-      </li> 
-    `;
-    this.$songs.insertAdjacentHTML('afterbegin',html);
+  hideLoading(){
+    this.$ct.querySelector('.loading').classList.add('hide');
   }
-  renderResult(json){
-    let list = json.data.song.list;
-    let html = list.map(list => {
+  // renderSinger(zhida){
+  //   let imgUrl = zhida.singermid || zhida.albummid;
+  //   let html = `
+  //     <li class="singer-info clearfix">
+  //       <span>
+  //         <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${imgUrl}.jpg?max_age=2592000">
+  //       </span>
+  //       <h6 class="result-title">${zhida.singername}</h6>
+  //       <p class="result-content text-hide">
+  //         ${zhida.singerid ? <span>单曲：${zhida.songnum}</span> : ''}
+  //         <span>专辑：${zhida.albumnum}</span>
+  //       </p>
+  //     </li> 
+  //   `;
+  //   this.$songs.insertAdjacentHTML('afterbegin',html);
+  // }
+  renderResult(lists){
+    let html = lists.map(list => {
       let artist = list.singer.map(singer => singer.name).join(' / ');
       return ` <li class="song clearfix">
       <i class="iconfont icon-music"></i>
