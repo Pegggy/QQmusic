ca29769dfbc9b7616bc65447d7f5bfdaf2dc1a71
penfan <fangpei9212@163.com>
2017-10-11 18:05:12 +0800

    增加歌手信息

--- scripts/search.js
+++ scripts/search.js
@@ -3,10 +3,28 @@ export class Search{
     this.$ct = ct;
     this.$input = this.$ct.querySelector('#search-input');
     this.$songs = this.$ct.querySelector('.search-content');
+    this.$cancelBtn = this.$ct.querySelector('.cancel-btn');
+    this.$input.addEventListener('focus',this.onFocus.bind(this));
+    this.$cancelBtn.addEventListener('click',this.onBtnClick.bind(this));
     this.$input.addEventListener('keyup',this.onKeyUp.bind(this));
     this.keyword = '';
     this.page = 1;
     this.perpage = 20;
+    this.cancel = false;
+  }
+  onFocus(event){
+    this.$ct.querySelector('.hot-search').classList.add('hide');
+    this.$ct.querySelector('.result-wrap').classList.remove('hide');
+    this.$cancelBtn.classList.remove('hide');
+  }
+  onBtnClick(event){
+    this.cancel = !this.cancel;
+    this.reset();
+    if(this.cancel){
+      this.$ct.querySelector('.hot-search').classList.remove('hide');
+      this.$ct.querySelector('.result-wrap').classList.add('hide');
+      this.$ct.querySelector('.cancel-btn').classList.add('hide');
+    }
   }
   onKeyUp(event){
     let keyword = event.target.value.trim();
@@ -20,10 +38,30 @@ export class Search{
     this.$songs.innerHTML = '';
   }
   search(keyword){
+    this.onFocus();
     this.keyword = keyword;
     fetch(`http://localhost:4000/search?keyword=${this.keyword}&page=${this.page}`)
       .then(res => res.json())
-      .then(json => this.renderResult(json))
+      .then(json => {
+        this.renderSinger(json.data.zhida);
+        this.renderResult(json)
+      })
+  }
+  renderSinger(zhidao){
+    let imgUrl = zhidao.singermid;
+    let html = `
+      <li class="singer-info clearfix">
+        <span>
+          <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${imgUrl}.jpg?max_age=2592000">
+        </span>
+        <h6 class="result-title">${zhidao.singername}</h6>
+        <p class="result-content text-hide">
+          <span>单曲：${zhidao.singerid}</span>
+          <span>专辑：${zhidao.albumnum}</span>
+        </p>
+      </li> 
+    `;
+    this.$songs.insertAdjacentHTML('afterbegin',html);
   }
   renderResult(json){
     let list = json.data.song.list;
