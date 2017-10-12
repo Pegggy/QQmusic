3dab8a80508aea0ed8912bc15eee26bd22ffc3f3
penfan <fangpei9212@163.com>
2017-10-12 14:05:49 +0800

    增加搜索结果 zhida

--- scripts/search.js
+++ scripts/search.js
@@ -30,6 +30,7 @@ export class Search{
       this.$ct.querySelector('.hot-search').classList.remove('hide');
       this.$ct.querySelector('.result-wrap').classList.add('hide');
       this.$ct.querySelector('.cancel-btn').classList.add('hide');
+      this.$ct.querySelector('.nomore').classList.add('hide');
     }
   }
   onKeyUp(event){
@@ -39,7 +40,7 @@ export class Search{
     this.search(keyword)
   }
   onScrollFn(event){
-    console.log(this.end);
+
     if(this.end) {
       this.$ct.querySelector('.nomore').classList.remove('hide');
       return window.removeEventListener('scroll',this.onscroll);
@@ -66,9 +67,9 @@ export class Search{
       .then(json => {
         this.page = json.data.song.curpage;
         this.end = (json.message === "no results");
-        return json.data.song.list;
+        return json.data;
       })
-      .then(list => this.renderResult(list))
+      .then(data => this.renderResult(data))
       .then(() => {
         this.isLoading = false;
         this.hideLoading();
@@ -80,23 +81,43 @@ export class Search{
   hideLoading(){
     this.$ct.querySelector('.loading').classList.add('hide');
   }
-  // renderSinger(zhida){
-  //   let imgUrl = zhida.singermid || zhida.albummid;
-  //   let html = `
-  //     <li class="singer-info clearfix">
-  //       <span>
-  //         <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${imgUrl}.jpg?max_age=2592000">
-  //       </span>
-  //       <h6 class="result-title">${zhida.singername}</h6>
-  //       <p class="result-content text-hide">
-  //         ${zhida.singerid ? <span>单曲：${zhida.songnum}</span> : ''}
-  //         <span>专辑：${zhida.albumnum}</span>
-  //       </p>
-  //     </li> 
-  //   `;
-  //   this.$songs.insertAdjacentHTML('afterbegin',html);
-  // }
-  renderResult(lists){
+  renderSinger(zhida,type){
+    let imgUrl;
+    if(type === 2){
+      imgUrl = zhida.singermid;
+      return  `
+        <li class="singer-info clearfix">
+          <span>
+            <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${imgUrl}.jpg?max_age=2592000">
+          </span>
+          <h6 class="result-title">${zhida.singername}</h6>
+          <p class="result-content text-hide">
+            <span>单曲：${zhida.songnum}</span>
+            <span>专辑：${zhida.albumnum}</span>
+          </p>
+        </li> 
+      `;
+    }else if( type === 3){
+      imgUrl = zhida.albummid;
+      return `
+        <li class="singer-info clearfix">
+          <span>
+            <img src="https://y.gtimg.cn/music/photo_new/T001R68x68M000${imgUrl}.jpg?max_age=2592000">
+          </span>
+          <h6 class="result-title">${zhida.albumname}</h6>
+          <p class="result-content text-hide">
+            <span>${zhida.singername}</span>
+          </p>
+        </li> 
+      `
+    }
+    else return '' 
+  }
+  renderResult(data){
+    let zhida = data.zhida;
+    let type = zhida.type;
+    let tophtml = this.renderSinger(zhida,type);
+    let lists = data.song.list;
     let html = lists.map(list => {
       let artist = list.singer.map(singer => singer.name).join(' / ');
       return ` <li class="song clearfix">
@@ -107,6 +128,6 @@ export class Search{
       </p>
     </li>
     `}).join('');
-    this.$songs.insertAdjacentHTML('beforeend',html);
+    this.$songs.insertAdjacentHTML('beforeend',tophtml + html);
   }
 }
