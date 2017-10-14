aea732b561a9af6c58e7904d5b989c877add9bf0
penfan <fangpei9212@163.com>
2017-10-14 18:41:04 +0800

    完成静态歌词滚动

--- scripts/lyrics.js
+++ scripts/lyrics.js
@@ -9,21 +9,46 @@ export default class LyricsPlayer{
     this.lyrics  = [];
     this.reset(this.formatText(this.text));
     this.render();
+    this.start();
   }
   start(){
-
+    this.pause();
+    this.intervalId = setInterval(this.update.bind(this),1000);
   }
   pause(){
-
+    clearInterval(this.intervalId);
+  }
+  update(){
+    this.elapsed += 1
+    if(this.index === this.lyrics.length - 1) return
+    for(let i = this.index + 1; i < this.lyrics.length; i++){
+      let seconds = this.getSeconds(this.lyrics[i])
+      if(this.elapsed === seconds && (!this.lyrics[i+1] || 
+        this.getSeconds(this.lyrics[i+1]) > this.elapsed)){
+          this.$lines.children[this.index].classList.remove('active');
+          this.$lines.children[i].classList.add('active');
+          this.index = i;
+        }
+      }
+    if(this.index > 2){
+      let topY = -(this.index - 2) * this.LINE_HEIGHT;
+      this.$lines.style.transform = `translateY(${topY}px)`
+    }
   }
   reset(text){
     this.lyrics = text.match(/^\[(\d{2}:\d{2}.\d{2})\].+$/gm) || [];
-
+    console.log(this.lyrics);
+  }
+  getSeconds(line){
+    return +line.replace(/\[(\d{2})\:(\d{2}).*/,(match,p1,p2)=>
+      (+p1) * 60 + (+p2)
+    )
   }
   render(){
     let html = this.lyrics.map( line =>`
       <p class="lyrics-line">${line.slice(10)}</p>
-    `).join('')
+    `).join('');
+    console.log(html);
     this.$lines.innerHTML = html;
   }
   formatText(text){
@@ -32,3 +57,4 @@ export default class LyricsPlayer{
     return div.innerText
   }
 }
+LyricsPlayer.prototype.LINE_HEIGHT = 42
